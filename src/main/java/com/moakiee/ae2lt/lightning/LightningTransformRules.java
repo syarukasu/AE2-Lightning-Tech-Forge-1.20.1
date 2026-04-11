2db5bf38abe844788e7c25df9aafa79149e25203
MOAKIEE <1324227985@qq.com>
2026-04-11 19:03:07 +0800

    feat: 移除成功概率相关逻辑和配置

--- src/main/java/com/moakiee/ae2lt/lightning/LightningTransformRules.java
+++ src/main/java/com/moakiee/ae2lt/lightning/LightningTransformRules.java
@@ -3,7 +3,6 @@ package com.moakiee.ae2lt.lightning;
 public final class LightningTransformRules {
     public static final double SEARCH_HORIZONTAL_RADIUS = 3.0D;
     public static final double SEARCH_VERTICAL_RADIUS = 2.0D;
-    public static final double SUCCESS_CHANCE = 0.75D;
     public static final long OUTPUT_PROTECTION_TICKS = 40L;
     public static final long PARTICIPANT_LOCK_TICKS = 2L;
 
