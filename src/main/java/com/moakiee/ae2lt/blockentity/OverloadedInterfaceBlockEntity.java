c2b6ffe9bbda1228970e016c00b5035ce1674295
MOAKIEE <1324227985@qq.com>
2026-04-25 17:08:27 +0800

    修复过载ME接口无法传输Mek化学品

--- src/main/java/com/moakiee/ae2lt/blockentity/OverloadedInterfaceBlockEntity.java
+++ src/main/java/com/moakiee/ae2lt/blockentity/OverloadedInterfaceBlockEntity.java
@@ -1054,7 +1054,7 @@ public class OverloadedInterfaceBlockEntity extends InterfaceBlockEntity
         if (AppFluxHelper.FE_KEY != null && keyType == AppFluxHelper.FE_KEY.getType()) {
             return false;
         }
-        return keyType == AEKeyType.items() || keyType == AEKeyType.fluids();
+        return true;
     }
 
     private void pollIOWheel(long now) {
