06a2581e14579653d0b562b1045e75ac2c5e51c4
MOAKIEE <1324227985@qq.com>
2026-05-20 12:19:04 +0800

    port: show lightning collector jade cooldown

--- src/main/java/com/moakiee/ae2lt/integration/jade/LightningCollectorJadeProvider.java
+++ src/main/java/com/moakiee/ae2lt/integration/jade/LightningCollectorJadeProvider.java
@@ -5,6 +5,8 @@ import com.moakiee.ae2lt.blockentity.LightningCollectorBlockEntity;
 import com.moakiee.ae2lt.registry.ModBlocks;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import snownee.jade.api.BlockAccessor;
 import snownee.jade.api.IBlockComponentProvider;
@@ -13,6 +15,7 @@ import snownee.jade.api.ITooltip;
 import snownee.jade.api.config.IPluginConfig;
 
 public class LightningCollectorJadeProvider implements IBlockComponentProvider, IServerDataProvider<BlockAccessor> {
+    private static final String TAG_COOLDOWN_TICKS = "CooldownTicks";
     private static final ResourceLocation UID =
             new ResourceLocation(AE2LightningTech.MODID, "lightning_collector");
 
@@ -24,7 +27,7 @@ public class LightningCollectorJadeProvider implements IBlockComponentProvider, 
     @Override
     public void appendServerData(CompoundTag data, BlockAccessor accessor) {
         if (accessor.getBlockEntity() instanceof LightningCollectorBlockEntity collector) {
-            data.putInt("CooldownTicks", collector.getCooldownTicks());
+            data.putInt(TAG_COOLDOWN_TICKS, collector.getCooldownTicks());
         }
     }
 
@@ -33,6 +36,18 @@ public class LightningCollectorJadeProvider implements IBlockComponentProvider, 
         if (!accessor.getBlockState().is(ModBlocks.LIGHTNING_COLLECTOR.get())) {
             return;
         }
+        var data = accessor.getServerData();
+        if (!data.contains(TAG_COOLDOWN_TICKS, Tag.TAG_INT)) {
+            return;
+        }
+
+        int cooldownTicks = data.getInt(TAG_COOLDOWN_TICKS);
+        if (cooldownTicks <= 0) {
+            return;
+        }
+
+        int cooldownSeconds = Math.max(1, (cooldownTicks + 19) / 20);
+        tooltip.add(Component.translatable("jade.ae2lt.lightning_collector.cooldown", cooldownSeconds));
     }
 }
 
